6ca510aa341b84a992f0e177459da8b4accfddde
ArhamAzhar1 <arham8718290@gmail.com>
2024-07-20 14:49:23 +0500

    Scripts for enemy

--- Assets/Scripts/States/Enemy.cs
+++ Assets/Scripts/States/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public StateMachine stateMachine;
+    private StateMachine stateMachine;
     private NavMeshAgent agent;
 
     public NavMeshAgent Agent { get => agent; }
@@ -14,7 +14,8 @@ public class Enemy : MonoBehaviour
     public Path path;
     private GameObject player;
     public float sightDistance = 20f;
-    public float fieldofView = 85f;
+    public float fieldOfView = 85f;
+    public float eyeHeight;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,7 +28,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CanSeePlayer();
     }
     public bool CanSeePlayer()
     {
@@ -35,8 +36,25 @@ public class Enemy : MonoBehaviour
         {
             if(Vector3.Distance(transform.position, player.transform.position) < sightDistance)
             {
-                Vector3 targetDirection = player.transform.position - transform.position;
+                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if (angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView)
+                {
+                    Ray ray = new Ray((transform.position + Vector3.up * eyeHeight), targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+                    if (Physics.Raycast(ray, out hitInfo, sightDistance))
+                    {
+                        if (hitInfo.transform.gameObject == player)
+                        {
+                            return true;
+                        }
+                    }
+
+                    Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+                }
+               
             }
         }
+        return false;
     }
 }
